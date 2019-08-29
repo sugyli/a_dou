a4d1d988de2a0d7e7fd2e48d15cec484dc6af65e
“liunxsugyli” <3101967255@qq.com>
2019-08-30 07:44:12 +0800

    0.31

--- db_tools/import_category_data.py
+++ db_tools/import_category_data.py
@@ -10,6 +10,20 @@ sys.path.insert(0, parent_thispath)
 sys.path.insert(0, os.path.join(parent_thispath, 'quanbenxiaoshuo'))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.production")
 django.setup()
+
+from albums.models import Category
+from db_tools.data.category_data import row_data
+
+for lev1_cat in row_data:
+    lev1_intance = Category()
+    lev1_intance.name = lev1_cat["name"]
+    lev1_intance.title=lev1_cat["title"]
+    lev1_intance.keywords=lev1_cat["keywords"]
+    lev1_intance.description=lev1_cat["description"]
+    lev1_intance.save()
+
+
+
 #
 # # #/Users/sugyil/quanbenxiaoshuo
 # # #DJANGO_SETTINGS_MODULE = 'config.settings.production'
