b3953da64ea52e48c7aeb543c4ea33a324067d40
“liunxsugyli” <3101967255@qq.com>
2019-08-30 13:32:41 +0800

    0.39

--- config/settings/production.py
+++ config/settings/production.py
@@ -179,19 +179,3 @@ LOGGING = {
 
 # Your stuff...
 # ------------------------------------------------------------------------------
-if DEBUG:
-    # django-debug-toolbar
-    # ------------------------------------------------------------------------------
-    # https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#prerequisites
-    INSTALLED_APPS+=["debug_toolbar"]  # noqa F405
-    # https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#middleware
-    MIDDLEWARE+=[
-        "debug_toolbar.middleware.DebugToolbarMiddleware"]  # noqa F405
-    # https://django-debug-toolbar.readthedocs.io/en/latest/configuration.html#debug-toolbar-config
-    DEBUG_TOOLBAR_CONFIG={
-        "DISABLE_PANELS": ["debug_toolbar.panels.redirects.RedirectsPanel"],
-        "SHOW_TEMPLATE_CONTEXT": True,
-        'SQL_WARNING_THRESHOLD': 2  # 数据库延迟时间 毫秒
-    }
-    # https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#internal-ips
-    INTERNAL_IPS=["*"]
