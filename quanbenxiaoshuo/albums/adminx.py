6e2279811dad7cd36693761f005e700ac776fce5
“liunxsugyli” <3101967255@qq.com>
2019-08-31 13:41:35 +0800

    0.651

--- quanbenxiaoshuo/albums/adminx.py
+++ quanbenxiaoshuo/albums/adminx.py
@@ -29,7 +29,7 @@ class CategoryAdmin(object):
 
 
 class AlbumAdmin(object):
-    list_display = ["name","is_tab","updated_at","apply_prove"]
+    list_display = ["name","is_tab","updated_at","push","apply_prove"]
     exclude = ["slug"]
     style_fields={"info": "ueditor"}
     list_editable=["is_tab"]
