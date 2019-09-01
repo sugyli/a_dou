02c6777205555b928834fc470ebb4d340f150046
“liunxsugyli” <3101967255@qq.com>
2019-09-01 12:21:24 +0800

    0.67

--- quanbenxiaoshuo/helpers.py
+++ quanbenxiaoshuo/helpers.py
@@ -1,14 +1,22 @@
-import html,re
+import html,re,os,sys
 from html.parser import HTMLParser
 
 
+filterstr = \
+"""
+落l霞x小x说s=Www*luoxia*com
+落l霞x小x说=Www*luoxia*com
+落l霞x小x说s
+落l霞x小x说
+Www*luoxia*com
+"""
 
 
 def __basereplace(htmlstr):
     #把HTML转义字符 反转义 html.escape转义
     htmlstr = html.unescape(htmlstr)
-    htmlstr = htmlstr.replace('\n', '')
     htmlstr = htmlstr.replace('\r', '')
+    htmlstr = htmlstr.replace('\t', '')
     re_stopwords=re.compile('\u3000', re.I)
     htmlstr =re_stopwords.sub('', htmlstr)
 
@@ -54,10 +62,11 @@ def descriptionreplace(htmlstr):
         return htmlstr
 
     htmlstr = __basereplace(htmlstr)
+    htmlstr = htmlstr.replace('\n', '')
 
     re_script = re.compile('<\s*script[^>]*>[^<]*<\s*/\s*script\s*>',re.I)  # Script
     re_style = re.compile('<\s*style[^>]*>[^<]*<\s*/\s*style\s*>', re.I)  # style
-    re_a=re.compile('<\s*a[^>]*>[^<]*<\s*/\s*a\s*>', re.I)  # a
+    re_a = re.compile('<\s*a[^>]*>[^<]*<\s*/\s*a\s*>', re.I)  # a
     htmlstr = re_script.sub('', htmlstr)
     htmlstr = re_style.sub('', htmlstr)
     htmlstr = re_a.sub('', htmlstr)
@@ -68,8 +77,7 @@ def descriptionreplace(htmlstr):
     #遗漏的HTML转义
     htmlstr = html.escape(htmlstr)
 
-    return htmlstr
-
+    return customfilterstr(htmlstr)
 
 
 def contentreplace(text ,out = True):
@@ -78,8 +86,7 @@ def contentreplace(text ,out = True):
     elif not text.strip():
         return text
 
-    text=text.replace('\r', '')
-    text=text.replace('\t', '')
+    text = __basereplace(text)
     re_script = re.compile('<\s*script[^>]*>[^<]*<\s*/\s*script\s*>',re.I)  # Script
     re_style = re.compile('<\s*style[^>]*>[^<]*<\s*/\s*style\s*>', re.I)  # style
     re_a=re.compile('<\s*a[^>]*>[^<]*<\s*/\s*a\s*>', re.I)  # a
@@ -96,6 +103,21 @@ def contentreplace(text ,out = True):
             if out:
                 html+=f"<p>{row}</p>"
             else:
-                html+=f"{row}\n"
+                html+=f"{row}\n\r"
 
     return html
+
+
+
+def customfilterstr(htmlstr,filterstr=filterstr):
+    filterstr = filterstr.split('\n')
+    filterstr = [row for row in filterstr if row.strip()]
+    #filterstr = list(set(filterstr))
+    filterstr = [i for n, i in enumerate(filterstr) if i not in filterstr[:n]]
+
+    for s in filterstr:
+        htmlstr = htmlstr.replace(s, '')
+
+    return htmlstr
+
+
