fd0dc2ea95f4349b170f078a59b78e08a64cfed8
“liunxsugyli” <3101967255@qq.com>
2019-09-16 13:10:59 +0800

    1.252

--- spider/spider/spiders/my2852_info.py
+++ spider/spider/spiders/my2852_info.py
@@ -37,8 +37,8 @@ class My2852InfoSpider(scrapy.Spider):
     def parse(self, response):
 
         try:
-            novel_dict = parse_info(response,'金萱')
-            albums=['金萱小说全集']
+            novel_dict = parse_info(response,'艾佟')
+            albums=['艾佟小说全集']
             tags=['言情小说']
 
             novel = Novel.objects.filter(**novel_dict).first()
