9cebbab8057fac5223ba95e80600cf0db601bc6b
“liunxsugyli” <3101967255@qq.com>
2019-09-18 14:36:34 +0800

    1.316

--- quanbenxiaoshuo/operation/models.py
+++ quanbenxiaoshuo/operation/models.py
@@ -49,8 +49,9 @@ class Compose(models.Model):
         return self.name
 
     def get_compose_url(self):
+        return '/'
 
-        return reverse('operation:compose', args=[self.slug])
+        #return reverse('operation:compose', args=[self.slug])
 
     def get_debug_compose_url(self):
         return reverse('operation:debugcompose', args=[self.slug])
