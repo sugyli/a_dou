bdfe47fc7def02b38825c4b5e5344a5526ee6677
“liunxsugyli” <3101967255@qq.com>
2019-10-10 11:58:26 +0800

    1.4481

--- spider/my2852_neirong.py
+++
@@ -1,8 +0,0 @@
-from scrapy.cmdline import execute
-import os,sys
-
-
-sys.path.append(os.path.abspath( os.path.dirname(__file__)))
-
-#execute('scrapy crawl luoxiaxiaoshuo'.split())
-execute('scrapy crawl my2852'.split())
